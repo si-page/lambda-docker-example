6dff9f525842af67dc76dd3f382cdacef7e9c23b
Simon Page <simon.page@krakentechnologies.ltd>
2021-02-14 21:19:24 +0000

    Add ECR Terraform config

---
+++ terraform/workspaces/services/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  version = "~> 3.1, != 3.14.0"
+
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region     = var.aws_region
+}
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "SiPage"
+
+    workspaces {
+      name = "lambda-docker-example-services"
+    }
+  }
+}
+
+resource "aws_ecr_repository" "lambda" {
+  name = "lambdafunctions"
+}
+
+
